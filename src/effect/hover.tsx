81a4f329830f46290a62274043c3b76b0c78f644
AdvancedAntiSkid <abel.herfort@gmail.com>
2024-11-20 20:22:36 +0100

    feat(effect): make mouse properties constant

--- src/effect/hover.tsx
+++ src/effect/hover.tsx
@@ -1,9 +1,12 @@
+const MOUSE_X = '--mouse-x';
+const MOUSE_Y = '--mouse-y';
+
 export const handleMouseHover = (e: any) => {
     for (const card of document.getElementsByClassName('card') as any) {
         const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
-        card.style.setProperty('--mouse-x', `${x}px`);
-        card.style.setProperty('--mouse-y', `${y}px`);
+        card.style.setProperty(MOUSE_X, `${x}px`);
+        card.style.setProperty(MOUSE_Y, `${y}px`);
     }
 };
